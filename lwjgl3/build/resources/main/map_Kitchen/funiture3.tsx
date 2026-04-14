dc2569b10c23edaed08773369d2a23d2f97e6ce7
Nguyen Tuan Hung <anhduc1612000@gmail.com>
2026-04-14 14:43:12 +0700

    Dọn dẹp hiện trường: Áp dụng .gitignore chuẩn

--- lwjgl3/build/resources/main/map_Kitchen/funiture3.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="funiture3" tilewidth="16" tileheight="16" tilecount="120" columns="12">
- <image source="pixelinterior_LRK_v1.1/decorations_LRK.png" width="192" height="160"/>
-</tileset>
